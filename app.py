691e7e7da26fa415a778019e2f9883133fa9ca26
David Padawer <d.padawer@gmail.com>
2021-02-07 16:42:28 -0800

    ugly but works

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from random import sample
+from classes.movie import Movie
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/newMovie', methods=["POST"])
+def populatePageWithNewMovie():
+    #print(getRandomMovie())
+    new_movie = get_random_movie()
+    return render_template("index.html", movie=new_movie.name)
+
+
+# list instead of set because random.sample
+movies = [
+Movie.iron_man(),
+Movie.hulk(),
+Movie.iron_man_2(),
+Movie.thor(),
+Movie.captain_america(),
+Movie.avengers(),
+Movie.iron_man_3(),
+Movie.thor_2(),
+Movie.captain_america_2(),
+Movie.guardians_of_the_galaxy(),
+Movie.avengers_2(),
+Movie.ant_man(),
+Movie.captain_america_3(),
+Movie.doctor_strange(),
+Movie.guardians_of_the_galaxy_2(),
+Movie.spider_man(),
+Movie.thor_3(),
+Movie.black_panther(),
+Movie.avengers_3(),
+Movie.ant_man_2(),
+Movie.captain_marvel(),
+Movie.avengers_4(),
+Movie.spider_man_2()]
+
+def get_random_movie():
+    return sample(movies, 1)[0]
---
+++ classes/movie.py
@@ -0,0 +1,95 @@
+class Movie:
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def iron_man(cls):
+        return cls("Iron Man")
+
+    @classmethod
+    def hulk(cls):
+        return cls("The Incredible Hulk")
+
+    @classmethod
+    def iron_man_2(cls):
+        return cls("Iron Man 2")
+
+    @classmethod
+    def thor(cls):
+        return cls("Thor")
+
+    @classmethod
+    def captain_america(cls):
+        return cls("Captain America: The First Avenger")
+
+    @classmethod
+    def avengers(cls):
+        return cls("Marvel's The Avengers")
+
+    @classmethod
+    def iron_man_3(cls):
+        return cls("Iron Man 3")
+
+    @classmethod
+    def thor_2(cls):
+        return cls("Thor: The Dark World")
+
+    @classmethod
+    def captain_america_2(cls):
+        return cls("Captain America: The Winter Soldier")
+
+    @classmethod
+    def guardians_of_the_galaxy(cls):
+        return cls("Guardians of the Galaxy")
+
+    @classmethod
+    def avengers_2(cls):
+        return cls("Avengers: Age of Ultron")
+
+    @classmethod
+    def ant_man(cls):
+        return cls("Ant-man")
+
+    @classmethod
+    def captain_america_3(cls):
+        return cls("Captain America: Civil War")
+
+    @classmethod
+    def doctor_strange(cls):
+        return cls("Doctor Strange")
+
+    @classmethod
+    def guardians_of_the_galaxy_2(cls):
+        return cls("Guardians of the Galaxy Vol. 2")
+
+    @classmethod
+    def spider_man(cls):
+        return cls("Spider-Man: Homecoming")
+
+    @classmethod
+    def thor_3(cls):
+        return cls("Thor: Ragnarok")
+
+    @classmethod
+    def black_panther(cls):
+        return cls("Black Panther")
+
+    @classmethod
+    def avengers_3(cls):
+        return cls("Avengers: Infinity War")
+
+    @classmethod
+    def ant_man_2(cls):
+        return cls("Ant-Man and the Wasp")
+
+    @classmethod
+    def captain_marvel(cls):
+        return cls("Captain Marvel")
+
+    @classmethod
+    def avengers_4(cls):
+        return cls("Avengers: Endgame")
+
+    @classmethod
+    def spider_man_2(cls):
+        return cls("Spider-Man: Far From Home")
